a85e14b60036e9c50810ddea35d5f41d8549cddf
dli <dli@v2.cluster.local>
2023-08-26 23:03:33 -0400

    Vector branch save

--- scripts/create_venv.sh
+++ scripts/create_venv.sh
@@ -1,6 +1,6 @@
 # Assumes that ROOTDIR is set and pointing to mm_masking root directory
 cd $ROOTDIR
-virtualenv venv
+python3 -m venv venv
 source venv/bin/activate
 pip install -r $ROOTDIR/requirements.txt
 deactivate
