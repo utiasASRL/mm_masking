422b8e26134b38807fac07b9abb45d3fe1596920
Daniil <lisus.daniil@gmail.com>
2025-01-23 14:38:33 -0500

    Removing need to pre-define ROOTDIR

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,5 +1,7 @@
 # Assumes that ROOTDIR is set and pointing to radar_topometric_localization root directory
-cd $ROOTDIR
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export ROOTDIR=$(dirname "$SCRIPT_DIR")
+
 docker build -t mm_masking_$(id -u) -f Dockerfile \
     --build-arg USERID=$(id -u) \
     --build-arg GROUPID=$(id -g) \
