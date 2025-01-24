70fe1f5d216c66042e1381d676f97bdee5a7308f
Daniil <lisus.daniil@gmail.com>
2025-01-24 16:58:42 -0500

    Changed package building to avoid RAM/CPU freezing

--- scripts/build_boreas_vtr_wrapper.sh
+++ scripts/build_boreas_vtr_wrapper.sh
@@ -4,6 +4,6 @@ export VTRSRC=$ROOTDIR/external/vtr3
 source /opt/ros/humble/setup.bash
 source ${VTRSRC}/main/install/setup.bash # source the vtr3 environment
 cd $ROOTDIR/external/boreas_vtr_wrapper # go to where boreas_vtr_wrapper is located
-colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
+MAKEFLAGS="-j$(($(nproc --all) / 2 + 1))" colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
 
 cd $ROOTDIR
